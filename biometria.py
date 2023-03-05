72d69c6a043abb5bb78be04b5ce63d796ef6b456
Juancho <juancrlos6@gmail.com>
2023-03-05 15:40:18 -0600

    Resalte de dientes

--- biometria.py
+++ biometria.py
@@ -28,25 +28,27 @@ def median(img):
     return new
 
 @jit
-def threshold(img):
+def threshold(img, perc):
     suma = 0
+    ths = np.ones_like(img)*img
+    sh = np.shape(ths)
     for i in range(256):
-        suma += np.count_nonzero(img==i)/(img.shape[0]*img.shape[1])
-        if suma > .2:
+        suma += np.count_nonzero(ths==i)/(sh[0]*sh[1])
+        if suma > perc:
             T = i
             break
     
-    for r in range(img.shape[0]):
-        for c in range(img.shape[1]):
-            if img[r,c] < T:
-                img[r,c] = 255
+    for r in range(sh[0]):
+        for c in range(sh[1]):
+            if ths[r,c] < T:
+                ths[r,c] = 255
             else:
-                img[r,c] = 0
+                ths[r,c] = 0
     """
     img[img<T] = 0
     img[img>=T] = 255
     """
-    return img
+    return ths
 
 def IMR(img):
     
@@ -83,6 +85,7 @@ def removeSpots(lbl, spots, ths):
     return lbl
 #@jit
 def ajuste(arr, perc):
+
     regionIMR = np.where(arr==255)
     sizeIMR = len(regionIMR[1])
     sampleSize = int(np.floor(perc*sizeIMR))
@@ -102,6 +105,18 @@ def sampleInfo(float(size), float(perc)):
     return int(np.floor(perc*size))
 """
 @njit
+def renormalize(img):
+    range1 = np.array([np.min(img), np.max(img)])
+    range2 = np.array([0.,255.])
+    ref = np.ones(np.shape(img))
+    out = np.empty_like(img)
+
+    delta1 = range1[1] - range1[0]
+    delta2 = range2[1] - range2[0]
+
+    return np.round((delta2*ref * (img - ref*range1[0]) / delta1) + ref*range2[0], 0, out)
+    #return np.round_((delta2 * (img - range1[0]) / delta1) + range2[0])
+@njit
 def sobel(img):
     m = np.shape(img)[0]-2
     n = np.shape(img)[1]-2
@@ -110,7 +125,7 @@ def sobel(img):
         for c in range(n):
             A = img[r:r+3, c:c+3]
             new[r,c] = kernelSobel(A)
-    return new
+    return renormalize(new)
 @njit
 def kernelSobel(img):
     Gx = np.array([[1,0,-1],
@@ -125,11 +140,13 @@ def kernelSobel(img):
     for r in range(sh[0]):
         for c in range(sh[1]):
             resX += img[r,c]*Gx[r,c]
-            resY += img[r,c]*Gy[r,c]
-    res = np.round_(np.sqrt(resX*resX + resY*resY))
-    if res>255:
-        res = 255
-    return res
+            #if resX>255:
+            #    resX = 255
+            #if resX<0:
+            #    resX = 0
+            #resY += img[r,c]*Gy[r,c]
+    #res = np.round_(np.sqrt(resX*resX + resY*resY))
+    return resX
 
 #@njit
 def curveIntensity(img, poly, offset, imrRange):
@@ -146,23 +163,44 @@ def curveIntensity(img, poly, offset, imrRange):
         result.append(totalIntensity)
     return result
 
+
+def gamma(img, gam, c=1): #0 < gam < 1, oscuros expandidos; gam > 1, claros expandidos
+    img = renormalize(c*img**gam)
+    return np.round_(img)
+
+def enhanceTeeth(img, gam, ths):
+    new = median(img)
+    new = sobel(sobel(new))
+    new = median(new)
+    new = gamma(new, gam)
+    return threshold(new, ths)
+
+@njit
+def negative(img):
+    return np.ones_like(img)*255-img
+
 im1 = '//home//juancho//Documents//Servicio//CIO//Imagenes//139001-17-M.png'
 im2 = '//home//juancho//Documents//Servicio//CIO//Imagenes//138785-21-M.png'
 im3 = '//home//juancho//Documents//Servicio//CIO//Imagenes//138943-20-M.png'
-im4 = '//home//juancho//Documents//Servicio//CIO//Imagenes//139008-22-H.png'
+#im4 = '//home//juancho//Documents//Servicio//CIO//Imagenes//139008-22-H.png'
+im5 = '//home//juancho//Documents//Servicio//CIO//Imagenes//139293-21-M.png'
 
-img = cv2.imread(im4, 0)
+img = cv2.imread(im1, 0)
 xSect = int(np.round_(.28*np.shape(img)[0]))
-ySect = int(np.round_(.15*np.shape(img)[1]))
+ySect = int(np.round_(.10*np.shape(img)[1]))
 clippedImg, offset = clip(img, xSect, ySect)
-
+#clippedImg = img[290:761,839:1071]
+#offset = [290,839]
 new = median(img)
-new = threshold(new)
+new = threshold(new, .15)
 imrImg = IMR(new)
 poly, imrRange = ajuste(imrImg, .15)
 
 #new = cv2.equalizeHist(cv2.Sobel(clippedImg, ddepth=-1,dx=1,dy=1,ksize=3))
-new = sobel(clippedImg)
+new = median(clippedImg)
+new = sobel(new)
+
+#new = new[290:761,839:1071]
 
 intensities = curveIntensity(new, poly, offset, imrRange)
 intensities1 = curveIntensity(clippedImg, poly, offset, imrRange)
@@ -192,25 +230,33 @@ print("t 50: ", fin50-inicio50)
 """
 
 f3 = plt.figure(3)
-f3.add_subplot(121)
+#f3.add_subplot(121)
 plt.scatter(range(len(intensities)), intensities)
-f3.add_subplot(122)
-plt.scatter(range(len(intensities1)), intensities1)
+plt.title("Sobel")
+#f3.add_subplot(122)
+#plt.scatter(range(len(intensities1)), intensities1)
+#plt.title("Original")
 
-minIntensidadIdx = np.argmin(intensities[np.argmax(intensities):])
+maxCoronas = np.argmax(intensities)
+minIntensidadIdx = np.argmin(intensities[maxCoronas:])
+maxDiastemas = np.argmax(intensities[minIntensidadIdx:])
 
 f1 = plt.figure(1)
 x = np.linspace(imrRange[0], imrRange[1], 1000)
 plt.imshow(img, cmap='gray')
-plt.plot(x, poly(x))
-plt.plot(x, poly(x)-minIntensidadIdx, color="r")
+plt.plot(x, poly(x), color="b")
+#plt.plot(x, poly(x)-minIntensidadIdx, color="r")
+plt.plot(x, poly(x)-maxCoronas, color="r")
 
 f2 = plt.figure(2)
-plt.imshow(imrImg, cmap='gray')
-#plt.plot(x-offset[0], poly(x)-offset[1])
-#plt.plot(x-offset[0], poly(x)-offset[1]-minIntensidadIdx, color="r")
-plt.plot(x, poly(x))
-plt.plot(x, poly(x)-minIntensidadIdx, color="r")
+plt.imshow(new, cmap='gray')
+plt.plot(x-offset[1], poly(x)-offset[0], color="b")
+plt.plot(x-offset[1], poly(x)-offset[0]-maxDiastemas, color="g")
+#plt.plot(x-offset[1], poly(x)-offset[0]-(4/5)*minIntensidadIdx, color="r")
+plt.plot(x-offset[1], poly(x)-offset[0]-maxCoronas, color="r")
+plt.legend(["Original", "Diastemas", "Corona"])
+#plt.plot(x, poly(x))
+#plt.plot(x, poly(x)-minIntensidadIdx, color="r")
 
 plt.show()
 #f, a = plt.subplots(1,1)
@@ -226,3 +272,7 @@ cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 '''
+"""
+(839,290)   (1070,290)
+(839,760)   (1070,760)
+"""
